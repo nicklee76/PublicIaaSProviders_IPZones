6ae0f669a2d65413d0df73cbeb9402991a26760b
nicklee76 <coolnicklee@hotmail.com>
2015-03-01 09:17:30 -0800

    added get_ids_using_name to search HALO policies using the name

--- build_ip_zones.py
+++ build_ip_zones.py
@@ -68,13 +68,18 @@ def build_ip_ranges_by_region(ip_ranges_json):
 
 
 def check_folders_and_files():
+    log_events(log_directory + 'script_logs.log', 'DEBUG', str(datetime.now()),
+               '[HALO] check_folders_and_files()')
     first_run = False
     if not os.path.exists(log_directory):
+        log_events(log_directory + 'script_logs.log', 'DEBUG', str(datetime.now()),
+                   'Failed to find the log_directory.')
         os.mkdir(log_directory)
         log_events(log_directory + 'script_logs.log', 'DEBUG', str(datetime.now()),
                    'Created log_directory and script execution log file.')
     if not os.path.exists(ip_ranges_directory):
-        print('[ERROR] No IP ranges folder found. Creating /AWS & /MS')
+        log_events(log_directory + 'script_logs.log', 'DEBUG', str(datetime.now()),
+                   'Failed to find ip_ranges_directory.')
         first_run = True
         os.mkdir(ip_ranges_directory)
         log_events(log_directory + 'script_logs.log', 'DEBUG', str(datetime.now()),
@@ -121,29 +126,29 @@ def get_access_token(url, query_string, headers):
 def halo_api_call(method, url, **kwargs):
     reply = requests.request(method, url, data=kwargs['data'], headers=kwargs['headers'], verify=False)
     log_events(log_directory + 'script_logs.log', 'DEBUG', str(datetime.now()),
-               '[HALO] HALO API call. \n\tmethod- %s\n\turl- %s\n\trequest_body-\n%s\n\theaders- %s'
+               '[HALO] HALO API call. \n\tmethod- %s\n\turl- %s\n\trequest_body-\n\t\t%s\n\theaders- %s'
                % (method, url, kwargs['data'], kwargs['headers']))
     return reply
 
 
 def create_halo_ip_zones(region_ip_list):
-   unique_id = region_ip_list['syncToken']
-
-   headers = get_headers()
-   # POST https://api.cloudpassage.com/v1/firewall_zones
-   for each in aws_region:
-       if each.lower() != 'synctoken':
-           request_body = {'firewall_zone': {'name': 'AWS-'+each+'['+unique_id+']',
-                                             'ip_address': ','.join(map(str, aws_region[each]))}}
-           status_code = '4xx'
-           while status_code != '201':
-               reply = halo_api_call('POST', halo_api_url + halo_api_version + '/firewall_zones',
-                                     data=json.dumps(request_body), headers=headers)
-               status_code = str(reply.status_code)
+    unique_id = region_ip_list['syncToken']
+    # POST https://api.cloudpassage.com/v1/firewall_zones
+    for each in aws_region:
+        if each.lower() != 'synctoken':
+            request_body = {'firewall_zone': {'name': 'AWS-'+each+'['+unique_id+']',
+                                              'ip_address': ','.join(map(str, aws_region[each]))}}
+            status_code = '4xx'
+            while status_code != '201':
+                reply = halo_api_call('POST', halo_api_url + halo_api_version + '/firewall_zones',
+                                      data=json.dumps(request_body), headers=headers)
+                status_code = str(reply.status_code)
 
 
 def get_ids_using_name(list, match_condition):
-    match_list=[]
+    log_events(log_directory + 'script_logs.log', 'DEBUG', str(datetime.now()),
+               '[HALO] get_ids_using_name(%s, %s)' % (list, match_condition))
+    match_dict={}
     for each in list:
         m=re.match(match_condition, each['name'])
         if m:
@@ -155,12 +160,35 @@ def get_ids_using_name(list, match_condition):
                            '[HALO] ID is None - %s' % each)
             else:
                 item_id = each["id"]
-            match_list.append(item_id)
-    return match_list
+            match_dict[each['name']] = item_id
+    #print('match_dict - %s' % match_dict)
+    log_events(log_directory + 'script_logs.log', 'DEBUG', str(datetime.now()),
+               '[HALO] match_dict - %s' % match_dict)
+    return match_dict
 
 
+def delete_old_ip_zones(dict):
+    #print ('delete_old_ip_zines(%s)' % dict)
+    for key, value in dict.iteritems():
+        status_code = '4xx'
+        while status_code != '204':
+            #DELETE https://api.cloudpassage.com/v1/firewall_zones/{id}
+            reply = halo_api_call('DELETE', halo_api_url+halo_api_version+'/firewall_zones/'+value, data=None, headers=headers)
+            if str(reply.status_code) == '204':
+                #print('%s, %s' % (key, value))
+                log_events(log_directory + 'script_logs.log', 'DEBUG', str(datetime.now()),
+                           '[HALO] DELETE request SUCCESSFUL for %s(id:%s)' % (key, value))
+            else:
+                log_events(log_directory + 'script_logs.log', 'DEBUG', str(datetime.now()),
+                           '[HALO] DELETE request FAILED for %s(id:%s).  Trying Again...' % (key, value))
+            status_code = str(reply.status_code)
+            #print('status_code - %s' % status_code)
 
+####################################################################################################################
 first_run = check_folders_and_files()
+
+headers = get_headers()
+sp_name = 'AWS'
 aws_ip_ranges = json.loads(open(aws_ip_ranges_directory + 'aws_ip_ranges.json', 'r').read())
 log_events(log_directory + 'script_logs.log', 'DEBUG', str(datetime.now()),
                'AWS IP Ranges -\n%s' % json.dumps(aws_ip_ranges, indent = 2))
@@ -173,19 +201,36 @@ if first_run == True:
     create_halo_ip_zones(aws_region)
 else:
     # not the first time getting the AWS ip range.
+    #print ("Not the first time")
     log_events(log_directory + 'script_logs.log', 'DEBUG', str(datetime.now()),
                'NOT the first time running the script')
     last_aws_synctoken = aws_ip_ranges['syncToken']
-    latest_aws_synctoken = get_file_from_internet(aws_ip_range_url).json()['syncToken']
+    print ('Last sync token %s' % last_aws_synctoken)
+    #latest_aws_synctoken = get_file_from_internet(aws_ip_range_url).json()['syncToken']
+    latest_aws_synctoken = '1424718133'
+    print ('Latest sync token %s' % latest_aws_synctoken)
 
-    if latest_aws_synctoken > last_aws_synctoken:
+    if int(latest_aws_synctoken) > int(last_aws_synctoken):
+        #print('starting for update')
         log_events(log_directory + '/script_logs.log', 'DEBUG', str(datetime.now()),
                    'New updated AWS IP ranges.  Old syncToken:' + last_aws_synctoken + ' New syncToken:' + latest_aws_synctoken)
+        # get all the firewall_zones
+        reply = halo_api_call('GET', halo_api_url+halo_api_version+'/firewall_zones', data=None, headers=headers)
+        #print json.dumps(reply.json()['firewall_zones'], indent = 2, sort_keys = True)
+        #print ('starting to delete')
+        to_be_deleted = get_ids_using_name(reply.json()['firewall_zones'], sp_name+'-(.+)\['+last_aws_synctoken+'\]')
+        print('To_be_deleted - %s' % to_be_deleted)
+        delete_old_ip_zones(to_be_deleted)
+        print('Building the new updated ip zones')
+        log_events(log_directory + '/script_logs.log', 'DEBUG', str(datetime.now()),
+                   'Building the new updated IP Zones')
         aws_region = build_ip_ranges_by_region(aws_ip_ranges)
+        log_events(log_directory + '/script_logs.log', 'DEBUG', str(datetime.now()),
+                   'Starting to create new HALO IP Zones')
         create_halo_ip_zones(aws_region)
     else:
         log_events(log_directory + '/script_logs.log', 'DEBUG', str(datetime.now()),
-                   'NO new updated AWS ip ranges.  Old syncToken:' + last_aws_synctoken + ' New syncToken:' + latest_aws_synctoken)
+                   'NO new updated AWS IP ranges.  Old syncToken:' + last_aws_synctoken + ' New syncToken:' + latest_aws_synctoken)
 
 
 
